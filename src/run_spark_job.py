27654042361c143ea6e56397fa8e0ae650cfc18a
Shankar Hadimani <shankar.hadimani@gmail.com>
2020-01-26 22:23:34 +0000

    new updates

--- src/run_spark_job.py
+++ src/run_spark_job.py
@@ -1,22 +1,20 @@
-from importlib import import_module
 import argparse
+import importlib
 import logging
+import os
 
 
-def run_spark_jobs(module_name, class_name, param_file):
+def run_spark_jobs(class_name, param_file):
     """
     Return a class instance from a string reference
-    :param module_name: The fully qualified  module name
-    :param class_name: The class name
+    :param class_name: The absolute class name
     :param param_file: configuration file path
     :return:
     """
-    print(module_name)
-    print(class_name)
-    print(param_file)
-
+    # split module and individual class names
+    module_name, class_name = class_name.rsplit('.', 1)
     try:
-        module_ = import_module(module_name)
+        module_ = importlib.import_module(module_name)
         try:
             class_ = getattr(module_, class_name)().run(param_file)
         except AttributeError:
@@ -28,17 +26,25 @@ def run_spark_jobs(module_name, class_name, param_file):
 
 if __name__ == '__main__':
 
-    # specify the yaml file from arg parse and handle rest
+    # specify the yaml filename from arg parse and handle rest
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument("-m", "--module_name",
-                        default="src.jobs.movies_under_genre", help="Spark job module name to be run")
 
-    parser.add_argument("-c", "--class_name",
-                        default="MovieUnderRating", help="Class name from the module to instantiated")
+    parser.add_argument("-class", "--class_name",
+                        default="src.jobs.movies_under_genre.MovieUnderRating",
+                        help="absolute class name - module.submodules.class")
 
-    parser.add_argument("-config", "--config_file",
-                        default="C:\\Users\\jassm\\PycharmProjects\\pyspark-asos\\config\\dev"
-                                "-config.yml", help="project configuration file for parameters other than environment "
-                                                    "variables")
+    # get configuration file path environment variable
+    try:
+        config_file = os.environ['CONFIG_FILE_PATH']
+        parser.add_argument("-config", "--config_file", default=config_file,
+                            help="project configuration/parameters file, other than environment variables")
+    except KeyError as e:
+        print("Cannot find config path in environment variables, so using default unless passed as an argument with "
+              "option --config : " + str(e))
+
+        parser.add_argument("-config", "--config_file",
+                            default="C:\\Users\\jassm\\PycharmProjects\\pyspark-asos\\config\\dev"
+                                    "-config.yml", help="project configuration/parameters file, other than environment "
+                                                        "variables")
     arg = parser.parse_args()
-    run_spark_jobs(arg.module_name, arg.class_name, arg.config_file)
+    run_spark_jobs(arg.class_name, arg.config_file)
--- src/test.py
+++ src/test.py
@@ -1,67 +1,33 @@
-from dependencies.spark import start_spark
-from pyspark.rdd import RDD
-from pyspark.sql import DataFrame, functions as F
-from pyspark.sql.types import StructField, StructType, IntegerType, StringType
-from src.extract_load import ExtractLoadFiles
-from src.config_helpers import LoadConfiguration
-import sys
-import argparse
+import importlib.util
 
 
-def main():
-    # We only specify the yaml file from arg parse and handle rest
-    parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument("-f", "--config_file", default="C:\\Users\\jassm\\PycharmProjects\\pyspark-asos\\config\\dev"
-                                                       "-config.yml", help="Configuration file to load.")
-    arg = parser.parse_args()
-
-    print(arg)
-    cfg = LoadConfiguration(arg.config_file)
-    params = cfg.yaml_loader()
-    in_ratings = params['FileSystem']['input']['ratings']
-    out_ratings = params['FileSystem']['output']['movies_avg_rating']
-    print(in_ratings)
-    print(out_ratings)
-    spark, log, config = start_spark(app_name='my_etl_job')
-
-    # log that main ETL job is starting
-    log.warn('etl_job is up-and-running')
-    el = ExtractLoadFiles()
-
-    read_data = el.rdd_extract_data(spark=spark, input_file_path=in_ratings)
-    trans_data = transform_data(spark, read_data, get_schema())
-    el.load_data(df=trans_data, partitioned=False,
-                 write_mode="Overwrite",
-                 output_file_path=out_ratings,
-                 file_header=True)
-
-    # log the success and terminate Spark application
-    log.warn('test_etl_job is finished')
-    spark.stop()
-    return None
+def check_module(module_name):
+    """
+    Checks if module can be imported without actually
+    importing it
+    """
+    module_spec = importlib.util.find_spec(module_name)
+    if module_spec is None:
+        print('Module: {} not found'.format(module_name))
+        return None
+    else:
+        print('Module: {} can be imported!'.format(module_name))
+        return module_spec
 
 
-def transform_data(spark, rdd: RDD, schema: StructType) -> DataFrame:
-    input_df = rdd.map(lambda x: x.split("::")).map(lambda x: [int(x[0]), int(x[1]), int(x[2]), int(x[3])])
-    map_schema = spark.createDataFrame(input_df, schema)
-    grp_df = map_schema.groupBy('UserID').agg({'MovieID': 'count', 'Ratings': 'avg'})
-    round_df = grp_df \
-        .withColumnRenamed('count(MovieID)', 'No of Movies') \
-        .withColumn('Average Rating', F.round('avg(Ratings)', 0))
-    df = round_df.select('UserID', 'No of Movies', 'Average Rating')
-    return df
-
-
-def get_schema() -> StructType:
-    ratings_schema = StructType([
-        StructField("UserID", IntegerType(), True),
-        StructField("MovieID", IntegerType(), True),
-        StructField("Ratings", IntegerType(), True),
-        StructField("Timestamp", StringType(), True)
-    ])
-    return ratings_schema
+def import_module_from_spec(module_spec):
+    """
+    Import the module via the passed in module specification
+    Returns the newly imported module
+    """
+    module = importlib.util.module_from_spec(module_spec)
+    module_spec.loader.exec_module(module)
+    return module
 
 
 if __name__ == '__main__':
-    main()
-
+    module_spec = check_module('fake_module')
+    module_spec = check_module('src.jobs.movies_avg_rating')
+    if module_spec:
+        module = import_module_from_spec(module_spec)
+        print(dir(module))
